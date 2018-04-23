075e9d2bcef8665d8ef382f21e808ca6fa8d6d67
Álvaro López García <33002461+KillerKing18@users.noreply.github.com>
2018-04-23 10:07:17 +0200

    Third Commit

--- TrafficSimulator/src/view/MainPanel.java
+++ TrafficSimulator/src/view/MainPanel.java
@@ -27,7 +27,7 @@ public class MainPanel extends JFrame implements TrafficSimulatorObserver {
 	private List<Event> _events; // observer update methods
 	private OutputStream _reportsOutputStream;
 	private File _inFile;
-	//PRUEBA COMMIT
+	//PRUEBA COMMIT2
 	
 	private final String EVENTS = "Events: ";
 	
